5f8c4d17893c943d08ce1c1710c2f4f8a7f9b58f
abdulwajeedali007 <abdulwajeedali007@gmail.com>
2022-12-10 23:56:31 +0530

    first commit for demo

---
+++ assests/script/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    nav: true,
+    dots: false,
+  });
+});
+
+// year adding in footer
+const year = new Date().getFullYear();
+document.querySelector(".year").innerText = `© ${year}. | ALLRIGHTS RESERVED`;
